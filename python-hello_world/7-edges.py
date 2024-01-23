645702f9a0feb8540f888cd85042afc7540e1fd6
Cedric002 <cedgnleblanc@gmail.com>
2024-01-23 00:30:10 -0800

    choose a letters

---
+++ python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = print(word[0:2])
+word_last_2 = print(word[7:8])
+middle_word = print(word[3:6])
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
