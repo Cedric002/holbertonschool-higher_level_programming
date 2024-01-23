ee2571101a228aa1125bf1c48f5b015efde294fb
Cedric002 <cedgnleblanc@gmail.com>
2024-01-23 00:55:12 -0800

    correction edges.py

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3 = print(word[0:2])
-word_last_2 = print(word[7:8])
-middle_word = print(word[3:6])
+word_first_3 = word[0:3]
+word_last_2 = word[7:9]
+middle_word = word[1:8]
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word}")
