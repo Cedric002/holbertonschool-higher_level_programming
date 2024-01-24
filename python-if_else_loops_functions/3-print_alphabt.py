d024028168459f647371735593c54765b2334224
Cedric002 <cedgnleblanc@gmail.com>
2024-01-24 11:22:43 -0800

    correction ASCII alphabet in lowercase except q & e 1.3

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for alphabets in range(97, 123):
-    if not (alphabets == 101 or alphabet == 113):
+    if not (alphabets == 101 or alphabets == 113):
         print("{}".format(chr(alphabets)), end="")
