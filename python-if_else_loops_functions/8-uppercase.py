878f3b031dc16cbcd1146bbcab2588b4fc91b540
Cedric002 <cedgnleblanc@gmail.com>
2024-01-25 12:29:02 -0800

    uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    result = ''
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            result += chr(ord(c) - 32)
+        else:
+            result += c
+    print(result)
+    print()
