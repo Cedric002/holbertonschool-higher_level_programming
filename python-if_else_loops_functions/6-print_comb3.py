f3bdd1e066244590c21c7397d8050b9799493d46
Cedric002 <cedgnleblanc@gmail.com>
2024-01-25 00:58:05 -0800

    correction print comb3 1.2

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for numbers1 in range(10):
     for numbers2 in range(numbers1+1, 10):
-        print(f"{numbers1}{numbers2}", end=", ")
+        print("{}{},".format(numbers1, numbers2), end=" ")
 print()
 
