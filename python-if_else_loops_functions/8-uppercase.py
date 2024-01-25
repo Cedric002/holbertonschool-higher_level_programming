a73c40f78b1785707ea6d7eeb4f0a530259281a3
Cedric002 <cedgnleblanc@gmail.com>
2024-01-25 12:40:40 -0800

    correction uppercase 1.4

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     result = ""
-    for c in str:
+    for c in range(len(str)):
         if 97 <= ord(c) <= 122:
             result += chr(ord(str[c]) - 32)
         else:
