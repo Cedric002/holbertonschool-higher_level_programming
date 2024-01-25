50db2d8eebbcc46e7c6de7bfcffda3873f8bdcc0
Cedric002 <cedgnleblanc@gmail.com>
2024-01-25 01:36:06 -0800

    correction print comb3 2

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for numbers1 in range(10):
     for numbers2 in range(10):
-        if numbers2 != 9 or numbers1 != 8:
+        if numbers2 == 9 or numbers1 == 8:
             print("{}{}".format(numbers1, numbers2), end=" ")
         elif (numbers1 != numbers2 and numbers1 < numbers2):
             print("{}{}{}".format(numbers1, numbers2, ", "), end=" ")
