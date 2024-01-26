9df470cf58edf2375b2f313305c572c833efcbcb
Cedric002 <cedgnleblanc@gmail.com>
2024-01-26 08:42:19 -0800

    correction 1-calculator 1.7

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 from calculator_1 import addition, substration, multiply, divide
 
-a = 10
-b = 5
+def main():
+    a = 10
+    b = 5
 
-print("{} + {}".format(a, b, addition(a, b)))
-print("{} - {}".format(a, b, subtration(a, b)))
-print("{} * {}".format(a, b, multiply(a, b)))
-print("{} / {}".format(a, b, divide(a, b)))
+    print("{} + {}".format(a, b, addition(a, b)))
+    print("{} - {}".format(a, b, subtration(a, b)))
+    print("{} * {}".format(a, b, multiply(a, b)))
+    print("{} / {}".format(a, b, divide(a, b)))
 
 if __name__ == "__main__":
     main()
