2a8b254584e17369140db376adaf8c7d47ffbaf3
Cedric002 <cedgnleblanc@gmail.com>
2024-01-26 08:09:35 -0800

    correction 1-calculator

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+from calculator_1 import add, subtract, multiply, divide
+
 a = 10
 b = 5
-
-from calculator_1 import add, subtract, multiply, divide
 
 print("Addition: {}".format(add(a, b)))
 print("Subtraction: {}".format(subtract(a, b)))
