dd9b16698f088612173af30c1e9f43ecec94b310
Cedric002 <cedgnleblanc@gmail.com>
2024-01-26 10:22:14 -0800

    correction hidden discovery 1.2

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,17 +2,9 @@
 import hidden_4
 
 def main():
-    pyc_path = "./hidden_4.pyc"
-
-    spec = importlib.util.spec_from_file_location("hidden_4", pyc_path)
-
-    hidden_4 = importlib.util.module_from_spec(spec)
-
-    spec.loader.exec_module(hidden_4)
-
     for name in dir(hidden_4):
         if not name.startswith("__"):
-            print(name)
+            print("{}".format(name))
 
 if __name__ == "__main__":
     main()
