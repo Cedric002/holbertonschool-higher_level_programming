3b27db0a083227e2b8c35720ca2c7bf8df72b7a0
Cedric002 <cedgnleblanc@gmail.com>
2024-01-26 08:44:10 -0800

    correction 1-calculator 1.8

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-from calculator_1 import addition, substration, multiply, divide
+from calculator_1 import add, sub, mul, div
 
 def main():
     a = 10
     b = 5
 
-    print("{} + {}".format(a, b, addition(a, b)))
-    print("{} - {}".format(a, b, subtration(a, b)))
-    print("{} * {}".format(a, b, multiply(a, b)))
-    print("{} / {}".format(a, b, divide(a, b)))
+    print("{} + {}".format(a, b, add(a, b)))
+    print("{} - {}".format(a, b, sub(a, b)))
+    print("{} * {}".format(a, b, mul(a, b)))
+    print("{} / {}".format(a, b, div(a, b)))
 
 if __name__ == "__main__":
     main()
