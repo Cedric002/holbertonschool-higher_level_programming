33b2f0898ac6b15aef2e0a38d20b69b112b2fe2d
Cedric002 <cedgnleblanc@gmail.com>
2024-01-26 08:33:05 -0800

    correction 1-calculator 1.5

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -2,13 +2,13 @@
 from calculator_1 import addition, subtraction, multiply, divide
 
 def main():
-a = 10
-b = 5
+    a = 10
+    b = 5
 
-print("{} + {}".format(addition(a, b)))
-print("{} - {}".format(subtraction(a, b)))
-print("{} * {}".format(multiply(a, b)))
-print("{} / {}".format(divide(a, b)))
+    print("{} + {}".format(addition(a, b)))
+    print("{} - {}".format(subtraction(a, b)))
+    print("{} * {}".format(multiply(a, b)))
+    print("{} / {}".format(divide(a, b)))
 
 if __name__ == "__main__":
     main()
