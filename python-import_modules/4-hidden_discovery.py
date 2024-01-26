c0dce2214a23580217e0743de9a9937a0838622c
Cedric002 <cedgnleblanc@gmail.com>
2024-01-26 10:18:28 -0800

    hidden discovery

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,18 @@
+import os
+import importlib.util
+
+def main():
+    pyc_path = "./hidden_4.pyc"
+
+    spec = importlib.util.spec_from_file_location("hidden_4", pyc_path)
+
+    hidden_4 = importlib.util.module_from_spec(spec)
+
+    spec.loader.exec_module(hidden_4)
+
+    for name in dir(hidden_4):
+        if not name.startswith("__"):
+            print(name)
+
+if __name__ == "__main__":
+    main()
