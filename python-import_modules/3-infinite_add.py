9d997b128710fdadcda6b5fea1b486248de24f36
Cedric002 <cedgnleblanc@gmail.com>
2024-01-26 09:57:29 -0800

    correction infinite 1.6

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-import sys
 
-def main():
+if __name__ == "__main__":
+    import sys
+
     args = sys.argv[1:]
     addition = 0
     for nb in args:
         addition += int(nb)
-    print("{}".format(addition))
-
-if __name__ == "__main__":
-    main()
+print("{}".format(addition))
