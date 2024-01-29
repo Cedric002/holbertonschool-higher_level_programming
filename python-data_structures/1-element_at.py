85b6bcfc6724fe318469c4683f9e436f0ff91105
Cedric002 <cedgnleblanc@gmail.com>
2024-01-29 02:09:04 -0800

    correction element at

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0 or > len(my_list):
-        return none
-    print("{}".format(idx))
+    if idx < 0:
+        return None
+    elif idx > len(my_list):
+        return None
+    else:
+        print("Element at index {:d} is {}".format(idx, my_list[idx]))
+        return my_list[idx]
