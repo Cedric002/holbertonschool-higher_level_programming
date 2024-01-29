432fbd52aade75ad31e828c9e28132b2ace26baa
Cedric002 <cedgnleblanc@gmail.com>
2024-01-29 05:29:41 -0800

    relace in list

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
+    if 0 <= idx < len(my_list):
+        new_list = my_list[:]
+        new_list[idx] = element
+        return new_list
+    else:
+        return my_list
