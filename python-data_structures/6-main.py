085f58546be2d000fd4aa546bd57a3e7ada9424d
Cedric002 <cedgnleblanc@gmail.com>
2024-01-30 00:55:18 -0800

    long line

--- python-data_structures/6-main.py
+++ python-data_structures/6-main.py
@@ -2,10 +2,10 @@
 print_matrix_integer = __import__('6-print_matrix_integer').print_matrix_integer
 
 matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+        ]
 
 print_matrix_integer(matrix)
 print("--")
