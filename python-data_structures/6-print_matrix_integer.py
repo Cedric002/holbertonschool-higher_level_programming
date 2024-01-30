862b713774522a2d2f4cae806db91a772737956f
Cedric002 <cedgnleblanc@gmail.com>
2024-01-30 02:12:18 -0800

    correction print matrix interger 1.7

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -5,4 +5,4 @@ def print_matrix_integer(matrix=[[]]):
             print("{:d}".format(row[i]), end='')
             if i != len(row) - 1:
                 print("", end=' ')
-        print()                
+        print()
