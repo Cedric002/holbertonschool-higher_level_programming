821fca029e7277c30a51b09515d6054e4e69ea20
Cedric002 <cedgnleblanc@gmail.com>
2024-01-30 01:27:06 -0800

    correction add tuple.py 1.3

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -5,7 +5,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     max_len = max(len(tuple_a), len(tuple_b))
 
     for nb in range(max_len):
-        result += ((tuple_a[i] if nb < len(tuple_a) else 0) +
-                   (tuple_b[i] if nb < len(tuple_b) else 0),)
+        result += ((tuple_a[nb] if nb < len(tuple_a) else 0) +
+                   (tuple_b[nb] if nb < len(tuple_b) else 0),)
 
     return result
