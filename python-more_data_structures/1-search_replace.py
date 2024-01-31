12872af7fd1c06aaf338d7f33ac6fc88d98e5963
Cedric002 <cedgnleblanc@gmail.com>
2024-01-31 00:49:12 -0800

    correction search replace

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    new_list = my_list[:]
     for element in range(len(my_list)):
         if my_list[element] == search:
             my_list[element] = replace
