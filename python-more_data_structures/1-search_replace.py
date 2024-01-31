ea335a179e9c762d2391226853e208a2854867ad
Cedric002 <cedgnleblanc@gmail.com>
2024-01-31 01:03:23 -0800

    correction search replace 1.3

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = my_list[:]
     for element in range(len(my_list) - 1):
         if my_list[element] == 89:
             my_list[element] = 2
