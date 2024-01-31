589db7be6520603c730a8c2144795b6a57c5a024
Cedric002 <cedgnleblanc@gmail.com>
2024-01-31 01:08:51 -0800

    correction search replace 1.4

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    for element in range(len(my_list) - 1):
-        if my_list[element] == 89:
-            my_list[element] = 2
-    return my_list
+    new_list = my_list[:]
+    for element in range(len(new_list) - 1):
+        if new_list[element] == search:
+            new_list[element] = replace
+    return new_list
