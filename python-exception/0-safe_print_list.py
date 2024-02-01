d4fa340aefbf0bbfd15dca5eaa7908ecc5c2182a
Cedric002 <cedgnleblanc@gmail.com>
2024-02-01 05:54:20 -0800

    add safe print list

---
+++ python-exception/0-safe_print_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+
+    for item in my_list:
+        try:
+            print(item, end=' ')
+            count += 1
+        except Exception:
+            continue
+
+        if count == x:
+            break
+
+    print()
+
+    return count
