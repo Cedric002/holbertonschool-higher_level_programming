9fb8e851fe04624229b03e85b6189df739aa62cf
Cedric002 <cedgnleblanc@gmail.com>
2024-02-01 05:57:32 -0800

    correction safe print list 1.2

--- python-exception/0-safe_print_list.py
+++ python-exception/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x = 0):
 
     for item in my_list:
         try:
-            print(item, end=' ')
+            print(item, end='')
             count += 1
         except Exception:
             continue
