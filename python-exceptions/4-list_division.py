796e995124dd09dee907802f11771991c203c150
Cedric002 <cedgnleblanc@gmail.com>
2024-02-02 06:08:04 -0800

    correction list division 1.5

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
-    if my_list_1 is not None or not my_list_2 is not None:
+    if my_list_1 is not None or my_list_2 is not None:
 
         for nb in range(0, list_length):
             result = 0
