10c9e275a393f794f3664cdb95b22f02e249c3c3
Cedric002 <cedgnleblanc@gmail.com>
2024-02-05 01:56:20 -0800

    add 1-square

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define a square.
+"""
+
+
+class Square:
+    """class Square with a private instance attribute size.
+    """
+    def __init__(self, size):
+        self.__size = alias
