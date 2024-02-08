fb20a255a7d08c924f74bb6b78044584cde280f0
Cedric002 <cedgnleblanc@gmail.com>
2024-02-08 10:26:01 -0800

    fix 2-rectangle 1.2

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -20,14 +20,14 @@ class Rectangle:
     perimeter: def __init__(self, width=0, height=0)
     """
 
-    class Rectangle:
-        def __init__(self, width=0, height=0):
-            self._width = width
-        self._height = height
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
-        return self._width
+        return self.__width
 
     @width.setter
     def width(self, value):
@@ -36,11 +36,11 @@ class Rectangle:
         elif value < 0:
             raise ValueError('width must be >= 0')
         else:
-            self._width = value
+            self.__width = value
 
     @property
     def height(self):
-        return self._height
+        return self.__height
 
     @height.setter
     def height(self, value):
@@ -49,12 +49,12 @@ class Rectangle:
         elif value < 0:
             raise ValueError('height must be >= 0')
         else:
-            self._height = value
+            self.__height = value
 
     def area(self):
-        return self._width * self._height
+        return self.__width * self.__height
 
     def perimeter(self):
-        if self._width == 0 or self._height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
-        return 2 * (self._width + self._height)
+        return 2 * (self.__width + self.__height)
