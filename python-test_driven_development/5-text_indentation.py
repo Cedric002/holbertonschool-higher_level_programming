adbefdd7a67fda4b6fc7765dc24b69e5837f84b8
Cedric002 <cedgnleblanc@gmail.com>
2024-02-11 09:58:56 -0800

    add text indentation

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def text_indentation(text):
+    """
+    Prints a text with 2 new lines after each of these
+    characters: ., ? and : .
+
+    text must be a string,
+    Otherwise TypeError (text must be a string)
+    No space at the beginning or at the end of each printed line.
+
+    Returns a text with : . ? and :
+    """
+
+    if not isinstance(text, str):
+        raise TypeError('text must be a string')
+
+    text = text.replace('.', '.\\n\\n').replace('?', '?\\n\\n').replace(':', ':\\n\\n')
+
+    text = text.strip()
+
+    print(text)
