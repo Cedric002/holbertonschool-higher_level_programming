0b00a28f321fe0567e884503e0ac909c59c533aa
Cedric002 <cedgnleblanc@gmail.com>
2024-02-11 10:19:12 -0800

    correction text indentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -14,7 +14,7 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError('text must be a string')
 
-    text = text.replace('.', '.\\n\\n').replace('?', '?\\n\\n').replace(':', ':\\n\\n')
+    text = text.replace('.', '.').replace('?', '?').replace(':', ':')
 
     text = text.strip()
 
