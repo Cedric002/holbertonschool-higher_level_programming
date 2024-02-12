daf680cb21fe381dbee3af1f414ffcd72e9d1356
Cedric002 <cedgnleblanc@gmail.com>
2024-02-12 05:36:46 -0800

    correction lookup 1.4

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def lookup(obj):
     """
+    tThis function returns the list of available attributes and
+    methods of an object.
+
+    obj (object): The object whose attributes and methods are to be listed.
+
     Returns the list of available attributes and methods of an object
     even the hidden ones.
     """
