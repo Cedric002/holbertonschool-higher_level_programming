ae2587848a4db89a60f0b6d56a8a673b5f12be0f
Cedric002 <cedgnleblanc@gmail.com>
2024-02-12 05:20:32 -0800

    correction lookup 1.2

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def lookup(obj):
-    attributes_and_methods = dir(obj)
-    filtered_list = [attr for attr in attributes_and_methods if not attr.startswith('_')]
-    return filtered_list
+    return dir(obj)
