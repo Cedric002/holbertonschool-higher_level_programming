cfc44438f712ad55121b95238d3f11212d1c6b96
Cedric002 <cedgnleblanc@gmail.com>
2024-02-13 05:29:11 -0800

    add base geometry n°3

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+The function print class BaseGeometry with
+public instance method: def area(self): .
+Oterwise message (Exception area() is not implemented).
+A public instance method: def integer_validator(self, name, value): that value:
+name is string,
+if value is not an integer: Oterwise TypeError (<name> must be an integer),
+if value <= 0: oterwise ValueError (<name> must be greater than 0).
+"""
+
+
+class BaseGeometry:
+    """
+    The classe define a geometrie.
+    """
+    def area(self):
+        raise NotImplementedError("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{name} must be an integer")
+        elif value <= 0:
+            raise ValueError("{name} must be greater than 0")
