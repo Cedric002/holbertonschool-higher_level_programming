5dc770efd40de44bd004c300b1f051f27381afec
Cedric002 <cedgnleblanc@gmail.com>
2024-02-14 00:42:19 -0800

    add rectangle

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+The function print class Rectangle that inherits from BaseGeometry with
+instantiation with width and height: def __init__(self, width, height):
+width and height must be private and
+width and height must be positive integers, validated by integer_validator
+"""
+
+
+class BaseGeometry:
+    """
+    The classe define a geometrie and have that inherits a rectangle
+    """
+    pass
+
+def integer_validator(value):
+    if not isinstance(value, int) or value <= 0:
+        raise ValueError('Value must be a positive integer')
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        integer_validator(width)
+        integer_validator(height)
+        self.__width = width
+        self.__height = height
