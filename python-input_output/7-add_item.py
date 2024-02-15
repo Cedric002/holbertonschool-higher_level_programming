6aff9965214af9cd875da1cf171aecb594bdba3c
Cedric002 <cedgnleblanc@gmail.com>
2024-02-15 05:43:03 -0800

    add add_item.json

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+The function creates an Object from a “JSON file.
+"""
+
+
+import sys
+import json
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+class ListManager:
+    """
+    Class responsible for managing a list and saving it to a JSON file.
+    """
+
+    def __init__(self, filename='add_item.json'):
+        self.filename = filename
+        self.my_list = []
+
+    def append_items(self, items):
+        self.my_list += items
+
+    def save_to_file(self):
+        save_to_json_file(self.my_list, self.filename)
+
+    def load_from_file(self):
+        return load_from_json_file(self.filename)
+
+def main():
+    args = sys.argv[1:]
+    list_manager = ListManager()
+    list_manager.append_items(args)
+    list_manager.save_to_file()
+
+if __name__ == "__main__":
+    main()
