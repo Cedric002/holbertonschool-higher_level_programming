193824831ee33cdb520a59ace5d726b1eddc1fce
Cedric002 <cedgnleblanc@gmail.com>
2024-02-15 01:16:58 -0800

    update read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""
+The function reads a text file and prints it to stdout.
+"""
+
+
 def read_file(filename=""):
+    """
+    Reads a UTF-8 encoded text file and prints its contents to stdout.
+    """
+
     with open(filename, "r", encoding="utf-8") as f:
         for line in f:
             print(line, end='')
