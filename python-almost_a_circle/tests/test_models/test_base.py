e6b93c27f5f546e30fb1d6f4596ebc2f26472584
Cedric002 <cedgnleblanc@gmail.com>
2024-02-19 06:04:06 -0800

    correction base and test base

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -4,26 +4,22 @@ unittest Base class
 """
 
 import unittest
-from base import Base
+from models.base import Base
 
 class TestBase(unittest.TestCase):
     """
     test from Base class
     """
 
-    def test_base_with_id(self):
-        base_instance = Base(id=5)
-        self.assertEqual(base_instance.id, 5)
+    def test_id_assignment(self):
+        base = Base(id=1)
+        self.assertEqual(base.id, 1)
 
-    def test_base_without_id(self):
-        base_instance = Base()
-        self.assertEqual(base_instance.id, 1)
-
-    def test_base_multiple_instances(self):
-        base_instance1 = Base()
-        base_instance2 = Base()
-        self.assertEqual(base_instance1.id, 1)
-        self.assertEqual(base_instance2.id, 2)
+    def test_id_auto_increment(self):
+        base1 = Base()
+        base2 = Base()
+        self.assertEqual(base1.id, 1)
+        self.assertEqual(base2.id, 2)
 
 if __name__ == '__main__':
     unittest.main()
