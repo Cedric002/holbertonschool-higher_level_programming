cebe961491581e2aedcdd19c9930feb8d20826d1
Cedric002 <cedgnleblanc@gmail.com>
2024-02-19 05:24:36 -0800

    correction test base

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -4,21 +4,26 @@ unittest Base class
 """
 
 import unittest
+from base import Base
 
 class TestBase(unittest.TestCase):
     """
     test from Base class
     """
 
-    def test_base_constructor(self):
-        base_instance = Base(1)
+    def test_base_with_id(self):
+        base_instance = Base(id=5)
+        self.assertEqual(base_instance.id, 5)
+
+    def test_base_without_id(self):
+        base_instance = Base()
         self.assertEqual(base_instance.id, 1)
 
-        base_instance = Base()
-        self.assertEqual(base_instance.id, 2)
-
-        base_instance = Base()
-        self.assertEqual(base_instance.id, 3)
+    def test_base_multiple_instances(self):
+        base_instance1 = Base()
+        base_instance2 = Base()
+        self.assertEqual(base_instance1.id, 1)
+        self.assertEqual(base_instance2.id, 2)
 
 if __name__ == '__main__':
     unittest.main()
