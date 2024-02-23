1abb1bc976a8d97d283d446f283f8cddf54a4b5a
Cedric <7520@holbertonstudents.com>
2024-02-23 08:51:30 +0000

    second update square and test_square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -2,6 +2,7 @@
 """
 Define a class Square.
 Update: adding the public getter and setter size.
+Update: adding the public method def update(self, *args, **kwargs)
 """
 
 from .rectangle import Rectangle
@@ -13,18 +14,32 @@ class Square(Rectangle):
     The setter assign width and height.
     The setter have the same value validation as
     the Rectangle for width and height.
+    The public method assigns attributes:
+    *args -> list of arguments (no-keyworded arguments) with
+    1st argument should be the id attribute
+    2nd argument should be the size attribute
+    3rd argument should be the x attribute
+    4th argument should be the y attribute
+    **kwargs -> double pointer to a dictionary: key/value (keyworded arguments)
+    **kwargs must be skipped if *args exists and not empty
     """
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(id, x, y, size, size)
 
-    @property
-    def size(self):
-        return self.width
-
-    @size.setter
-    def size(self, value):
-        self.set_width(value)
-        self.set_height(value)
+    def update(self, *args, **kwargs):
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                if hasattr(self, key):
+                    setattr(self, key, value)
 
     def __str__(self):
         return "[Square] ({}) {}/{} - {}".format(
--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -5,13 +5,22 @@ import unittest
 from models.square import Square
 
 class TestSquare(unittest.TestCase):
-    def test_size_getter_and_setter(self):
+    def test_update(self):
         s = Square(5)
-        self.assertEqual(s.size, 5)
-        s.size = 10
+        s.update(1, 10, 2, 3)
+        self.assertEqual(s.id, 1)
         self.assertEqual(s.size, 10)
-        with self.assertRaises(Exception):
-            s.size = "9"
+        self.assertEqual(s.x, 2)
+        self.assertEqual(s.y, 3)
+
+        s.update(x=4, y=5, id=2)
+        self.assertEqual(s.x, 4)
+        self.assertEqual(s.y, 5)
+        self.assertEqual(s.id, 2)
+
+        s.update(size=6, id=3)
+        self.assertEqual(s.size, 6)
+        self.assertEqual(s.id, 3)
 
 if __name__ == '__main__':
     unittest.main()
