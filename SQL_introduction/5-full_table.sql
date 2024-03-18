4e55a04a60b8deb339426c100f18ff30e46166cd
Cedric002 <cedgnleblanc@gmail.com>
2024-03-18 14:27:32 +0100

    update2 5-full table

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,14 +1,2 @@
 -- print a requested description of the table
-SELECT 
-    COLUMN_NAME AS 'Field',
-    DATA_TYPE AS 'Type',
-    IS_NULLABLE AS 'Null',
-    COLUMN_KEY AS 'Key',
-    COLUMN_DEFAULT AS 'Default',
-    EXTRA AS 'Extra',
-    COLUMN_COMMENT AS 'Comment'
-FROM 
-    mysql
-WHERE 
-    TABLE_SCHEMA = 'hbtn_0c_0' AND 
-    TABLE_NAME = 'first_table';
+SHOW CREATE TABLE first_table;
