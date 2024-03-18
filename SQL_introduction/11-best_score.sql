730dff1e806f6292f654541ce2a35c76e6ee15a3
Cedric002 <cedgnleblanc@gmail.com>
2024-03-18 15:29:16 +0100

    update 11-best score

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 --  lists all records with a score >= 10 in the 2nd table
-SELECT name, score FROM second_table WHERE score >= 10 ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
