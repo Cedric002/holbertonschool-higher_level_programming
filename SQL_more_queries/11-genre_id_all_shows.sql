6c76d43a77e6cf3a0b6da29d9109442970b92bfd
Cedric002 <cedgnleblanc@gmail.com>
2024-03-20 10:45:09 +0100

    add 10-genre id all shows

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,7 +1,7 @@
 -- Import the database dump from hbtn_0d_tvshows to server
--- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
+-- lists all shows contained in hbtn_0d_tvshows
 -- sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
-SELECT tv_shows.title, tv_show_genres.genre_id
+SELECT tv_shows.title, IFNULL(tv_show_genres.genre_id, 'NULL') AS genre_id
 FROM tv_shows, tv_show_genres
 WHERE tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
