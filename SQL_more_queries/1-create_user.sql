d1c6f1af367af0ba0b6647d27dde0722c2dbf478
Cedric002 <cedgnleblanc@gmail.com>
2024-03-20 09:32:31 +0100

    update5 1-create user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,9 @@
 -- creates the MySQL server user user_0d_1 with all privileges for user_0d_1
 -- and password should be set to user_0d_1_pwd
-CREATE USER 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
+SELECT EXISTS (SELECT DISTINCT user FROM mysql.user WHERE user = 'user_0d_1') as is_user;
+
+IF NOT EXISTS (SELECT DISTINCT user FROM mysql.user WHERE user = 'user_0d_1') THEN
+    CREATE USER 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd';
+    GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%' WITH GRANT OPTION;
+    FLUSH PRIVILEGES;
+END IF;
