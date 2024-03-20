d1e27cdb117d7885b24ec7408dd059aad985405b
Cedric002 <cedgnleblanc@gmail.com>
2024-03-20 09:31:19 +0100

    update4 1-create user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -2,3 +2,4 @@
 -- and password should be set to user_0d_1_pwd
 CREATE USER 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
