f41682739013559cf046fa7c7c878d01b37211a4
Cedric002 <cedgnleblanc@gmail.com>
2024-03-21 15:56:53 +0100

    add 2-my filer states

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Lists all states with a name starting with N (upper N) from the database hbtn_0e_0_us
+Connect to a MySQL server running on localhost at port 3306
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+   # Get the command-line arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connect to the MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username, passwd=mysql_password, db=database_name)
+    cursor = db.cursor()
+
+    # Construct the SQL query using the provided state name
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cursor.execute(query, (state_name,))
+
+    # Fetch and display the results
+    for row in cursor.fetchall():
+        print(row)
+
+    # Close the database connection
+    db.close()
