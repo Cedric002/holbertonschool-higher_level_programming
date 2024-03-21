d3d6ff5c96deaabe46c4e6c8b9964bb437d43089
Cedric002 <cedgnleblanc@gmail.com>
2024-03-21 14:43:24 +0100

    update 1-filter_states

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -8,13 +8,7 @@ import sys
 
 if __name__ == "__main__":
     # Connect to the database
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
-    )
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     # Create a cursor
     cur = db.cursor()
