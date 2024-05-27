824d24a8487cc57d411e8ed5f87f87a5eadd453c
Cedric002 <cedgnleblanc@gmail.com>
2024-05-27 15:40:17 +0200

    fix 8-script.js

--- javascript-dom_manipulation/8-script.js
+++ javascript-dom_manipulation/8-script.js
@@ -1,10 +1,14 @@
-const helloElement = document.getElementById("hello");
+function fetchAndDisplayHello() {
 
-fetch("https://hellosalut.stefanbohacek.dev/?lang=fr")
-  .then((response) => response.json())
-  .then((data) => {
-    helloElement.textContent = data.hello;
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-  });
+  fetch("https://hellosalut.stefanbohacek.dev/?lang=fr")
+    .then((response) => response.json())
+    .then((data) => {
+      const helloElement = document.getElementById("hello");
+      helloElement.textContent = data.hello;
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+window.onload = fetchAndDisplayHello;
